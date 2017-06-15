744629b679087bb52dd1fb84751f3b33532f92c9
Lukas <lukasdragon@live.ca>
2017-06-14 22:19:01 -0700

    New site (#1)
    Brand new site!

---
+++ scripts/coinConfig.js
@@ -0,0 +1,14 @@
+var coin = "palladium";
+
+var coinLogo = "images/logo.jpg";
+
+var coinDetails = "PalladiumCoin is an open decentralized cryptocurrency, based upon the total estimated value of all the chemical-element palladium found within the local solar system. Anyone interested can join the currency’s network and take part in currency development. PalladiumCoin offers safe and secure global transactions, offering the convenience of nearly instant feeless money transactions.";
+
+var currentVersion = "1.5.1";
+
+var downloads = [
+    { name: "Windows Executables", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.1/executables.zip", faicon: "fa-terminal" },
+    { name: "Windows GUI Wallet", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.1/Gui.Wallet.zip", faicon: "fa-laptop" },
+    { name: "Logos", "url": "downloads/logos.zip", faicon: "fa-file-image-o" },
+];
+
