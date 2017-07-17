e1c55c9d3ebed1a2f0223ee21243dacc0255460c
Lukas <lukasdragon@live.ca>
2017-07-17 11:02:37 -0700

    Update coinConfig.js

--- scripts/coinConfig.js
+++ scripts/coinConfig.js
@@ -4,11 +4,12 @@ var coinLogo = "http://palladiumco.in/images/logo.png";
 
 var coinDetails = "PalladiumCoin is an open decentralized cryptocurrency, based upon the total estimated value of all the chemical-element palladium found within the local solar system. Anyone interested can join the currency’s network and take part in currency development. PalladiumCoin offers safe and secure global transactions, offering the convenience of nearly instant feeless money transactions.";
 
-var currentVersion = "1.5.2";
+var currentVersion = "1.5.3";
 
 var downloads = [
-    { name: "Windows Executables", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.2/executables.zip", faicon: "fa-terminal" },
-    { name: "Windows GUI Wallet", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.2/Gui.Wallet.zip", faicon: "fa-laptop" },
-	{ name: "Logos", "url": "http://palladiumco.in/downloads/LogoFiles.zip", faicon: "fa-file-image-o" },
+    { name: "Windows Executables", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.3/executables.zip", faicon: "fa-terminal" },
+    { name: "Windows GUI Wallet", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.3/gui.wallet.zip", faicon: "fa-laptop" },
+    { name: "Ubuntu Executables", "url": "https://github.com/PalladiumCo1n/palladium/releases/download/1.5.3/ubuntuexecutables.zip", faicon: "fa-terminal" },
+    { name: "Logos", "url": "http://palladiumco.in/downloads/LogoFiles.zip", faicon: "fa-file-image-o" },
 ];
 
